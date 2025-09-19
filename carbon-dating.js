11eb09a6e7a2269a3aef6d3c40d523837404e175
13-arch <Iron_Mike@tutamail.com>
2025-09-19 13:23:52 +0300

    Add files via upload

---
+++ carbon-dating.js
@@ -0,0 +1,28 @@
+const { NotImplementedError } = require('../lib');
+
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+
+/**
+ * Determine the age of archeological find by using
+ * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
+ *
+ * @param {String} sampleActivity string representation of current activity
+ * @return {Number | Boolean} calculated age in years or false
+ * in case of incorrect sampleActivity
+ *
+ * @example
+ *
+ * dateSample('1') => 22387
+ * dateSample('WOOT!') => false
+ *
+ */
+function dateSample(sampleActivity) {
+    const a = parseFloat(sampleActivity);
+    if (typeof sampleActivity !== 'string' || isNaN(a) || a <= 0 || a > MODERN_ACTIVITY) return false;
+    return Math.ceil(Math.log(MODERN_ACTIVITY / a) / (Math.LN2 / HALF_LIFE_PERIOD));
+  }
+
+module.exports = {
+  dateSample
+};
